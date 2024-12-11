2506c218f0f661de795e4498087c9cf3905ae811
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2024-12-11 11:02:01 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -11,20 +11,28 @@ while IFS= read -r tf_file; do
     # Move into the directory containing main.tf
     pushd "$dir" >/dev/null || continue
 
-    if ! terraform init -no-color; then
-        echo "Terraform init failed in $dir" | tee -a "$LOGFILE"
-        popd >/dev/null
-        continue
+    if [ -f ".ignore_init" ]; then
+        echo "Skipping terraform init in $dir"
+    else
+        if ! terraform init -no-color; then
+            echo "Terraform init failed in $dir" | tee -a "$LOGFILE"
+            popd >/dev/null
+            continue
+        fi
     fi
 
-    if ! terraform validate -no-color; then
-        echo "Terraform validate failed in $dir" | tee -a "$LOGFILE"
-        popd >/dev/null
-        continue
+    if [ -f ".ignore_validate" ]; then
+        echo "Skipping terraform validate in $dir"
+    else
+        if ! terraform validate -no-color; then
+            echo "Terraform validate failed in $dir" | tee -a "$LOGFILE"
+            popd >/dev/null
+            continue
+        fi
     fi
 
     if [ -f ".ignore_plan" ]; then
-        echo "Skipping terraform plan in $dir because .ignore_plan file is present"
+        echo "Skipping terraform plan in $dir"
     else
         if ! terraform plan -no-color; then
             echo "Terraform plan failed in $dir" | tee -a "$LOGFILE"
@@ -38,6 +46,6 @@ while IFS= read -r tf_file; do
     # Return to the previous directory
     popd >/dev/null
 
-done < <(find "$SEARCH_DIR" -type f -name main.tf)
+done < <(find "$SEARCH_DIR" -type f -name main.tf -not -path "*/.terraform/*")
 
 echo "Script complete. Any failures were logged in $LOGFILE."
