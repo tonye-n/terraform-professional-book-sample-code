259e9002f901686f38360c520e3f7cbb97e5f717
tonye-n <tonye_n@msn.com>
2025-05-19 13:39:09 +0100

    final chapter 8

--- aws-2/chapter-08-refactoring/modules/security-group/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "security_group" {
-  value = {id = aws_security_group.this.id}
-  description = "The ID of the security group"
-}
--- aws-2/chapter-08-refactoring/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to deploy the resources"
-  default     = "eu-west-1"
-}
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
